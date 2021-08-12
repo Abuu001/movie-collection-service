d4671acf73563c19be8c159fb9a9927c1eeeb7cf
Abuu001 <abulugz@gmail.com>
2021-08-13 00:30:43 +0300

    md

---
+++ Movie-discovery-server/src/test/java/io/lugonzo/moviediscoveryserver/MovieDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lugonzo.moviediscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
