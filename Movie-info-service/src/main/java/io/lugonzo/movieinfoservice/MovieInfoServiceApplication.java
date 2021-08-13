138443ab1486ac94d7557288a66e0e9588ee5c11
Abuu001 <abulugz@gmail.com>
2021-08-13 22:13:57 +0300

    configured eurekaclient

--- Movie-info-service/src/main/java/io/lugonzo/movieinfoservice/MovieInfoServiceApplication.java
+++ Movie-info-service/src/main/java/io/lugonzo/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,11 +2,11 @@ package io.lugonzo.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class MovieInfoServiceApplication {
+ public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
