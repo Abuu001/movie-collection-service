110342a5dbe34346a273d9db8f4809ef2d3ffa85
Abuu001 <abulugz@gmail.com>
2021-08-17 00:16:04 +0300

    configured circuit breaker

--- Movie-catalogue-service/src/main/java/io/lugonzo/moviecatalogue/controller/MovieCatalogueResource.java
+++ Movie-catalogue-service/src/main/java/io/lugonzo/moviecatalogue/controller/MovieCatalogueResource.java
@@ -1,5 +1,6 @@
 package io.lugonzo.moviecatalogue.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import io.lugonzo.moviecatalogue.model.CatalogueItem;
 import io.lugonzo.moviecatalogue.model.Movie;
 import io.lugonzo.moviecatalogue.model.Rating;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,10 +28,12 @@ public class MovieCatalogueResource {
     private WebClient.Builder webClientBuilder;
 
     @GetMapping("/{userId}")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalogMthd") //is circuit breaker that breaks whenever an error has occurred .prevents whole system go down when there is a prob in one microservice
     public List<CatalogueItem> getCatalogue(@PathVariable("userId") String userId){
 
-        UserRating Allrating = restTemplate.getForObject("http://movie-ratings-data-service/api/v1/ratingsdata/users/"+ userId, UserRating.class);
-
+        UserRating Allrating = restTemplate.getForObject("http://movie-ratings-data-service/api/v1/ratingsdata/"+ userId, UserRating.class);
+        System.out.println("==============================================");
+        System.out.println(Allrating);
         return Allrating.getUserrating().stream().map(rating -> {
            Movie movie = restTemplate.getForObject("http://movie-info-service/api/v1/movies/"+ rating.getMovieId().toString(), Movie.class);
 
@@ -40,8 +44,19 @@ public class MovieCatalogueResource {
                     .retrieve()
                     .bodyToMono(Movie.class) ;
                */
-             return new CatalogueItem(movie.getName(),"test123",rating.getRating());
+             return new CatalogueItem(movie.getName(),
+                     movie.getDescription(),
+                     rating.getRating(),
+                     movie.getLanguage(),
+                     movie.getPopularity(),
+                     movie.getStatus(),
+                     movie.getVoteCount());
 
         }).collect(Collectors.toList());
+    }
+
+    //this method runs as fallback whenever the circuit breaks
+    public List<CatalogueItem> getFallbackCatalogMthd(@PathVariable("userId") String userId){
+        return Arrays.asList(new CatalogueItem("No movie","",0,"N/A",0,"N/A","N/A"));
     }
 }
