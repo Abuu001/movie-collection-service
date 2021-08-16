6203b7761f0bbb0cae70b84dd0e9945290fd0840
Abuu001 <abulugz@gmail.com>
2021-08-17 00:15:43 +0300

    configured classpath to have hysterix circuit breaker

--- Movie-catalogue-service/src/main/java/io/lugonzo/moviecatalogue/MovieCatalogueServiceApplication.java
+++ Movie-catalogue-service/src/main/java/io/lugonzo/moviecatalogue/MovieCatalogueServiceApplication.java
@@ -2,20 +2,25 @@ package io.lugonzo.moviecatalogue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class MovieCatalogueServiceApplication {
 
 	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate(){
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(3000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 
 	//Alternive for restTemplate
