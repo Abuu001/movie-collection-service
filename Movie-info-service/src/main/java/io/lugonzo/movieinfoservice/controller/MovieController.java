51eb763bd4509795e7a27a0c0d2062e0b5ab571c
Abuu001 <abulugz@gmail.com>
2021-08-17 00:15:06 +0300

    md

--- Movie-info-service/src/main/java/io/lugonzo/movieinfoservice/controller/MovieController.java
+++ Movie-info-service/src/main/java/io/lugonzo/movieinfoservice/controller/MovieController.java
@@ -1,23 +1,38 @@
 package io.lugonzo.movieinfoservice.controller;
 
 import io.lugonzo.movieinfoservice.model.Movie;
+import io.lugonzo.movieinfoservice.model.MovieSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId){
 
-        var movie1 = Movie.builder()
-                .movieId(movieId)
-                .name("The CHI")
-                .build();
+        var movieSummary =restTemplate.getForObject(
+                "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey, MovieSummary.class
+        );
 
-        return movie1;
+        return new Movie(movieId,movieSummary.getTitle() ,
+                movieSummary.getOverview(),
+                movieSummary.getLanguage(),
+                movieSummary.getPopularity(),
+                movieSummary.getStatus(),
+                movieSummary.getVoteCount()
+        );
     }
 }
