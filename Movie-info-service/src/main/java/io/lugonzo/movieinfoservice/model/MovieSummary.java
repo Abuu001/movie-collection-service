1c945a45c9cca0a83614b9edc368d416048f9bf4
Abuu001 <abulugz@gmail.com>
2021-08-16 21:28:28 +0300

    movie summary props

--- Movie-info-service/src/main/java/io/lugonzo/movieinfoservice/model/MovieSummary.java
+++ Movie-info-service/src/main/java/io/lugonzo/movieinfoservice/model/MovieSummary.java
@@ -1,2 +1,19 @@
-package io.lugonzo.movieinfoservice.controller;public class MovieSummary {
+package io.lugonzo.movieinfoservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MovieSummary {
+    private String title;
+    private String overview;
+ /*   private String language;
+    private Integer popularity;
+    private String status;
+    private String voteCount;*/
 }
