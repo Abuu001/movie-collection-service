0b0bacd8b7228970763f43a9d0624cd0b70ed4cb
Abuu001 <abulugz@gmail.com>
2021-08-18 00:22:52 +0300

    md

--- Movie-catalogue-service/src/main/java/io/lugonzo/moviecatalogue/controller/MovieCatalogueResource.java
+++ Movie-catalogue-service/src/main/java/io/lugonzo/moviecatalogue/controller/MovieCatalogueResource.java
@@ -5,15 +5,14 @@ import io.lugonzo.moviecatalogue.model.CatalogueItem;
 import io.lugonzo.moviecatalogue.model.Movie;
 import io.lugonzo.moviecatalogue.model.Rating;
 import io.lugonzo.moviecatalogue.model.UserRating;
+import io.lugonzo.moviecatalogue.service.MovieInfoService;
+import io.lugonzo.moviecatalogue.service.UserRatingInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,41 +21,20 @@ import java.util.stream.Collectors;
 public class MovieCatalogueResource {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private MovieInfoService movieInfoService;
 
     @Autowired
-    private WebClient.Builder webClientBuilder;
+    private UserRatingInfoService userRatingInfoService;
 
     @GetMapping("/{userId}")
-    @HystrixCommand(fallbackMethod = "getFallbackCatalogMthd") //is circuit breaker that breaks whenever an error has occurred .prevents whole system go down when there is a prob in one microservice
     public List<CatalogueItem> getCatalogue(@PathVariable("userId") String userId){
 
-        UserRating Allrating = restTemplate.getForObject("http://movie-ratings-data-service/api/v1/ratingsdata/"+ userId, UserRating.class);
-        System.out.println("==============================================");
-        System.out.println(Allrating);
-        return Allrating.getUserrating().stream().map(rating -> {
-           Movie movie = restTemplate.getForObject("http://movie-info-service/api/v1/movies/"+ rating.getMovieId().toString(), Movie.class);
+        UserRating userRating = userRatingInfoService.getUserRating(userId);
 
-            //Alternative to restTemplate 👇👇
-           /* Movie movie = webClientBuilder.build()
-                    .get()
-                    .uri("http://localhost:3003/api/v1/movies/"+ rating.getMovieId().toString())
-                    .retrieve()
-                    .bodyToMono(Movie.class) ;
-               */
-             return new CatalogueItem(movie.getName(),
-                     movie.getDescription(),
-                     rating.getRating(),
-                     movie.getLanguage(),
-                     movie.getPopularity(),
-                     movie.getStatus(),
-                     movie.getVoteCount());
-
-        }).collect(Collectors.toList());
+        return userRating.getUserrating().stream()
+                .map(rating -> movieInfoService.getCatalogItem(rating)
+        ).collect(Collectors.toList());
     }
 
-    //this method runs as fallback whenever the circuit breaks
-    public List<CatalogueItem> getFallbackCatalogMthd(@PathVariable("userId") String userId){
-        return Arrays.asList(new CatalogueItem("No movie","",0,"N/A",0,"N/A","N/A"));
-    }
 }
+
