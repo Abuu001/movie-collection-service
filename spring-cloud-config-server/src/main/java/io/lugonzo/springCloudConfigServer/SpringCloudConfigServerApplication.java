168b91152eeb32000a4639e705ab1f0ea225fd4f
Abuu001 <abulugz@gmail.com>
2021-08-19 22:33:12 +0300

    config server

--- spring-cloud-config-server/src/main/java/io/lugonzo/springCloudConfigServer/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/io/lugonzo/springCloudConfigServer/SpringCloudConfigServerApplication.java
@@ -2,6 +2,7 @@ package io.lugonzo.springCloudConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
